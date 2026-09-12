bb344a8e392d6372790170608c995e37ea7b3872
chavepixnubank42-cell <chavepixnubank42@gmail.com>
2026-09-12 12:56:23 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'controle-diario-v9';
+var CACHE_NAME = 'entrega-facil-v6';
 var ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(event){
